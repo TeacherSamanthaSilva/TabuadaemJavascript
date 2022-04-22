6aa5f65913a7088995c18c1996770ce949a841a9
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-04-21 23:17:57 -0300

    finalizei incremento
    Nesse commit finalizaei incremento

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function tabuada(){
         while(c< 10){
             let item = document.createElement(`option`);
             item.text = `${num} X ${c} = ${num*c}`;
+            tab.appendChild(item)
+            c = c+1;
         }
         
     }
