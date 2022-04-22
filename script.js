2f690b5dde0e65b4dd2045ca0c21254fe4340d89
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-04-21 23:16:47 -0300

    finalizando calculo
    Nesse commit finalizei o calculo da tabuada

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function tabuada(){
+    let num = document.getElementById(`txtn`);
+    let tab = document.getElementById(`seltabuada`);
+    if(num.value.lenght == 0){
+
+        window.alert("Digite um número qualquer");
+        
+
+    }else{
+
+        let n = Number(num.value);
+        let c=1;
+        while(c< 10){
+            let item = document.createElement(`option`);
+            item.text = `${num} X ${c} = ${num*c}`;
+        }
+        
+    }
+
     
 }
